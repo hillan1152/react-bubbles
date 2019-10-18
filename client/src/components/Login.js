a9735396638f5140e8a13f33a0c8d2fd7ca20490
Anthony Hill <hillan1152@gmail.com>
2019-10-18 11:09:45 -0400

    set up axiosAuth and working on Login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { axiosAuth } from '../utils/axiosAuth';
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  let [info, setInfo] = useState({
+    username:'', 
+    password: ''
+  })
+
+  useEffect({
+    setInfo
+  }, [])
+
+  let changeHandler = () => {
+    
+  }
+
+  let login = e => {
+    e.preventDefault();
+    axiosAuth()
+      .post('/api/login', info)
+      .then(res => console.log(res))
+      .catch(err => console.log(err.response))
+  }
+  
+
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={login}>
+        <input
+          type="text"
+          name="username"
+          value={info.username}
+          handleChanges={changeHandler}
+        />
+      </form>
     </>
   );
 };

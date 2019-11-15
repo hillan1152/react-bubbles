fa42692b0a3767d84af82698215fbf6b397a70ad
Anthony Hill <hillan1152@gmail.com>
2019-11-15 12:07:22 -0500

    completed login, private route, and bubble page

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import {axiosWithAuth} from '../utils/axiosWithAuth';
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
@@ -8,7 +9,11 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
-
+  useEffect(() => {
+    axiosWithAuth().get('/api/colors', colorList)
+      .then(res => setColorList(res.data))
+      .catch(err => console.log(err.response))
+  }, [])
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Login = () => {
+
+// Components
+import {axiosWithAuth} from '../utils/axiosWithAuth';
+
+const initialState = {
+  username: '',
+  password: undefined
+}
+
+
+const Login = (props) => {
+  const [input, setInput] = useState(initialState)
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const login = (e) => {
+    e.preventDefault();
+    axiosWithAuth().post('/api/login', { username: 'Lambda', password: '1234' })
+      .then(res => {
+        console.log('Login Res', res.data.payload)
+        localStorage.setItem('token', res.data.payload)
+        props.history.push('/bubblepage')
+      })
+      .catch(err => console.log("Login Error", err.response))
+  }
+
+  const handleChanges = e => {
+    setInput({...input, [e.target.name]:e.target.value})
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={login}>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            onChange={handleChanges}
+            value={input.username}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={handleChanges}
+            value={input.password}
+          />
+          <button>Login</button>
+      </form>
     </>
   );
 };

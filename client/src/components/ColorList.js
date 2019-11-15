9b19bb78c3db71d918aafd4602a059cace645d38
Anthony Hill <hillan1152@gmail.com>
2019-11-15 12:19:42 -0500

    Finally got delete to work

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+import {axiosWithAuth} from '../utils/axiosWithAuth';
+
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+  console.log("Colors Log",colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -25,6 +27,14 @@ const ColorList = ({ colors, updateColors }) => {
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth().delete(`/api/colors/${color.id}`, colorToEdit)
+      .then(res => {
+        console.log('Delete Is Working', res.data)
+        updateColors(colors.filter(click => click.id !== color.id))
+
+    })
+      .catch(err => console.log('Delete Error', err.response))
+
   };
 
   return (
